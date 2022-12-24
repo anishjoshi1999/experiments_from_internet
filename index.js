9cb5b94412eae9609de8a9a9cbc2a47260fd588e
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-12-24 08:48:29 +0545

    Basic pagination API

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-const express = require("express");
-const app = express();
-const path = require('path')
-const ejsMate = require('ejs-mate')
-const Person = require('./models/schema');
-const dotenv = require("dotenv").config()
-const mongoose = require("mongoose")
-const port = 5000;
+const express = require('express')
+const app = express()
+const paginatedResults = require('./middleware')
 
-// Configurations
-app.set('views', path.join(__dirname, 'views'))
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-// Connection with mongodb Atlas
-const conn_str = `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@experiment.rwpnw8r.mongodb.net/?retryWrites=true&w=majority`
-mongoose.set('strictQuery', false);
-mongoose.connect(
-    conn_str,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, (err) => {
-        if (err) {
-            console.log("error in connection");
-        } else {
-            console.log("mongodb is connected");
-        }
-    });
-
-
-//Routes
-app.get('/', (req, res) => {
-    res.render("home")
+const users = []
+for (let i = 1; i <= 15; i++) {
+    users.push({ id: i, name: `User ${i}` })
+}
+// users?page=1&limit=5
+app.get('/users', paginatedResults(users), (req, res) => {
+    res.json(res.paginatedResults)
 })
-app.listen(port, () => {
-    console.log("starting the server");
-});
-//the server object listens on port 8080
+
+
+app.listen(3000)
